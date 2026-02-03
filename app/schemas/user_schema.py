b5f74c9fb71cc1701570d1364b35b5022ddb1b1c
alanbispo <alannmg1@gmail.com>
2026-02-03 17:02:53 -0300

    feat: adiciona regras de validação para cadastro de usuário

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,12 +1,36 @@
 from app.extensions import ma
 from app.models.user_model import UserModel
+from marshmallow import fields, validate, ValidationError
+
+def validate_not_admin(value):
+    if value.lower() == 'admin':
+        raise ValidationError("O nome de usuário 'admin' é proibido.")
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = UserModel
         load_instance = True
-        ordered = True
+        include_fk = True
         exclude = ("password",)
+
+    username = fields.String(
+        required=True, 
+        validate=[
+            validate.Length(min=3, max=50, error="O nome deve ter entre 3 e 50 caracteres."),
+            validate_not_admin
+        ]
+    )
+    
+    email = fields.Email(
+        required=True, 
+        error_messages={"invalid": "Formato de e-mail inválido."}
+    )
+
+    password = fields.String(
+        required=True, 
+        load_only=True, 
+        validate=[validate.Length(min=8, error="A senha deve ter pelo menos 8 caracteres.")]
+    )
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
